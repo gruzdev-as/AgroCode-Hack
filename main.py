44d708032c288507f595aecf11edc1acc4d0b25b
lightsource <m2302070@edu.misis.ru>
2024-11-24 09:01:10 +0300

    fix imports

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from scripts import get_weather_data, generate_train_dataset, lama_train
+from scripts.generate_train_dataset import get_train_dataset
+from scripts.get_weather_data import get_weather_data
+from scripts.lama_train import train_lama
 
 # потом подтереть эти комменты
 # добавить аргпарсер, в ламе параметры n_threads, n_folds, random_state, timeout, target_name по желанию, также сам путь к /data наверное
@@ -6,6 +8,6 @@ if __name__ == '__main__':
     # тут чисто функция которая получает погодные данные и сохраняет их в /data
     get_weather_data()
     # тут функция собирает статистики, генерит pca фичи и к этому всему добавляет погодную дату и сохраняет финальный трейн датасет
-    generate_train_dataset()
+    get_train_dataset()
     # обучение модели на полученном датасете, выводятся метрики, фичимп, а также сохраняется полный отчет обучения
-    lama_train()
+    train_lama()
